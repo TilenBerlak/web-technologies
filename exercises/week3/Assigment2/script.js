3fd33986b20cc59aaf9d76d066f60ca4934093d8
Tilen Berlak <berlak.tilen@gmail.com>
2020-03-29 18:21:28 +0200

    Add week4 files

--- exercises/week3/Assigment2/script.js
+++ exercises/week3/Assigment2/script.js
@@ -1,21 +1,12 @@
 "use strict";
 
-let participantId = 0;
-let participantArray = [];
-
 function domRemoveParticipant(event) {
     // TODO
     const table = document.querySelector("#participant-table");
 
-    let j = 0;
     for(let i = 0; i < table.rows.length; i++) {
         if(i != 0 && confirm("Are you sure you want to delete " + table.rows[i].cells[0].innerHTML + "?")) {            
             table.deleteRow(i);
-            let dataJSON = JSON.parse(localStorage.getItem("data"));
-            delete dataJSON[j++];
-            console.log(dataJSON);
-            let arrayJSON = JSON.stringify(dataJSON);
-            localStorage.setItem("data", arrayJSON);
         }
     }
 }
@@ -28,11 +19,10 @@ function domAddParticipant(participant) {
     table.appendChild(tr);
 
     for (const key in participant) {
-        if(key != "id") {
-            const td = document.createElement("td");
-            td.innerText = participant[key];
-            tr.appendChild(td);
-        }
+        const td = document.createElement("td");
+        td.innerText = participant[key];
+        tr.appendChild(td);
+        window.localStorage.setItem(key, participant[key]);
     }
 }
 
@@ -48,16 +38,10 @@ function addParticipant(event) {
 
     // Create participant object
     const participant = {
-        id: participantId++,
         first: first,
         last: last,
         role: role
     };
-
-    // Local Storage
-    participantArray.push(participant);
-    let arrayJSON = JSON.stringify(participantArray);
-    localStorage.setItem("data", arrayJSON);
 
     // Add participant to the HTML
     domAddParticipant(participant);
@@ -69,23 +53,6 @@ function addParticipant(event) {
 document.addEventListener("DOMContentLoaded", () => {
     // This function is run after the page contents have been loaded
     // Put your initialization code here
-    const table = document.getElementById("participant-table");
-    let dataJSON = JSON.parse(localStorage.getItem("data"));
-    
-    if (dataJSON != null && dataJSON[0] != null) {
-        for(let i = 0; i < dataJSON.length; i++) {
-            const participant = {
-                id: dataJSON[i]["id"],
-                first: dataJSON[i]["first"],
-                last: dataJSON[i]["last"],
-                role: dataJSON[i]["role"]
-            };
-    
-            domAddParticipant(participant);
-                 
-        }
-    }
-
     document.getElementById("addButton").onclick = addParticipant;
-    table.ondblclick = domRemoveParticipant;
+    document.getElementById("participant-table").ondblclick = domRemoveParticipant;
 })
